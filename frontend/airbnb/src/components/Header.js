aa72575a7ee34fa0d95aca1820c1a99a1c97c470
AlaaAlTawil <Alaa.m.Tawil@outlook.com>
2022-07-02 12:57:21 +0300

    creating react project

---
+++ frontend/airbnb/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function Header() {
+    return (  
+        <div>
+            <div><img src='..../public/airbnblogo.png'></img></div>
+            <div></div>
+            <div></div>
+        </div>
+    );
+}
+
+export default Header;
