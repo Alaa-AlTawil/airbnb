96b2bc0c9d3b08bac314fa5c5de142eefa3748d4
AlaaAlTawil <Alaa.m.Tawil@outlook.com>
2022-07-02 16:29:02 +0300

    creating get all places api

--- backend/airbnb/app/Http/Controllers/PlaceController.php
+++ backend/airbnb/app/Http/Controllers/PlaceController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Place;
 class PlaceController extends Controller
 {
     function getAllPlaces(){
         
+            $places=Place::all();
+            return response()->json([
+                "status" => "success",
+                "items" => $place
+            ],200);
+    
+        
     }
+  
 }
