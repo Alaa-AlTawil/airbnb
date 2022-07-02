4e7e9188bfd34425637b2510ab9841eec1a9581d
AlaaAlTawil <Alaa.m.Tawil@outlook.com>
2022-07-02 13:45:45 +0300

    stoping at the mid side of the header

--- frontend/airbnb/src/components/Header.js
+++ frontend/airbnb/src/components/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import logo from '../assets/airbnblogo.png'
 function Header() {
     return (  
-        <div>
-            <div><img src='..../public/airbnblogo.png'></img></div>
-            <div></div>
-            <div></div>
+        <div className="header">
+            <div className="left-side-header"><img height={50} src={logo}></img></div>
+            <div className="mid-side-header">
+                <div className='mid-side-content'>
+                    <div>Anywhere</div>
+                    <div>Any week</div>
+                    <div>Add guests</div>
+                    <div><img ></img></div>
+                </div>
+            </div>
+            <div className="right-side-header"></div>
         </div>
     );
 }
