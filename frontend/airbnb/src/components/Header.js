aac129937f498329e24cf8df617473baa6dab545
AlaaAlTawil <Alaa.m.Tawil@outlook.com>
2022-07-02 15:32:30 +0300

    completing  the header

--- frontend/airbnb/src/components/Header.js
+++ frontend/airbnb/src/components/Header.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import logo from '../assets/airbnblogo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch, faGlobe, faBars, faUser, faCircleUser } from '@fortawesome/free-solid-svg-icons'
+
 function Header() {
     return (  
         <div className="header">
             <div className="left-side-header"><img height={50} src={logo}></img></div>
             <div className="mid-side-header">
                 <div className='mid-side-content'>
-                    <div>Anywhere</div>
-                    <div>Any week</div>
+                    <div><strong>Anywhere</strong></div>
+                    <div><strong>Any week</strong></div>
                     <div>Add guests</div>
-                    <div><img ></img></div>
+                    <button><FontAwesomeIcon icon={faSearch} color='white' /></button>
                 </div>
             </div>
-            <div className="right-side-header"></div>
+            <div className="right-side-header">
+                <div><strong>Become a Host</strong></div>
+                <div><FontAwesomeIcon icon={faGlobe} color='black' /></div>
+                <button><div><FontAwesomeIcon icon={faBars} color='black' /></div><div><FontAwesomeIcon icon={faCircleUser} color='gray'/></div></button>
+            </div>
         </div>
     );
 }
