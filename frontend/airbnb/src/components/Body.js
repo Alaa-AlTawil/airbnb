c6653e9d77b14d9137d27cad3b2f09b00e731253
AlaaAlTawil <Alaa.m.Tawil@outlook.com>
2022-07-03 02:31:06 +0300

    updating database places table

--- frontend/airbnb/src/components/Body.js
+++ frontend/airbnb/src/components/Body.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Card from './Card';
 function Body() {
+
+    
     return ( 
         <div className='cards'>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
+            <Card title='lebanon' rate='3.5' desc='95 Kilometers away' date='AUG 22-25' price='250'/>
+            <Card title='Brazil' rate='3' desc='195 Kilometers away' date='AUG 22-25' price='250'/>
+            <Card title='Germany' rate='4.5' desc='275 Kilometers away' date='AUG 22-25' price='250'/>
+            <Card title='Spain' rate='5' desc='97 Kilometers away' date='AUG 2-5' price='250'/>
+            <Card title='UAE' rate='2.5' desc='100 Kilometers away' date='AUG 25-28' price='250'/>
+            <Card title='Jordan' rate='4.5' desc='175 Kilometers away' date='AUG 20-23' price='250'/>
         </div>
      );
 }
--- frontend/airbnb/src/components/Card.js
+++ frontend/airbnb/src/components/Card.js
@@ -2,17 +2,17 @@ import React from 'react';
 import rest from '../assets/try.jpeg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
-function Card({title,rate,desc,date,price}) {
+function Card({title,rate,desc,date,price,img}) {
     return ( 
         <div className='card'>
             <div><img src={rest}/></div>
             <div>
-                <div>{title}</div>
+                <div><strong>{title}</strong></div>
                 <div>{rate}<div className='fontawesome'><FontAwesomeIcon icon={faStar} color='rgb(46, 45, 45)' /></div></div>
             </div>
             <div>{desc}</div>
             <div>{date}</div>
-            <div>{price}</div>
+            <div><strong>${price}</strong> night</div>
 
         </div>
      );
