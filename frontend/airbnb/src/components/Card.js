0c8bdab8500d40305cb3d0726ec76682f96c8c8c
AlaaAlTawil <Alaa.m.Tawil@outlook.com>
2022-07-03 15:26:22 +0300

    creating heart for favorite

--- frontend/airbnb/src/components/Card.js
+++ frontend/airbnb/src/components/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import rest from '../assets/try.jpeg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { faStar,faHeart } from '@fortawesome/free-solid-svg-icons'
 function Card({title,rate,desc,date,price,img,key}) {
 
     return ( 
         <div className='card' key={key}>
-            <div><img src={img}/></div>
+            <div><FontAwesomeIcon className='heart' icon={faHeart} color='rgb(46, 45, 45)' size='lg' /><img src={img}/></div>
             <div>
                 <div><strong>{title}</strong></div>
                 <div>{rate}<div className='fontawesome'><FontAwesomeIcon icon={faStar} color='rgb(46, 45, 45)' /></div></div>
