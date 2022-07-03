1d5dbfe6a51226ef3b5831556e9de1b36305c772
AlaaAlTawil <Alaa.m.Tawil@outlook.com>
2022-07-03 17:48:50 +0300

    initializing the admin page

--- frontend/airbnb/src/components/Admin.js
+++ frontend/airbnb/src/components/Admin.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
+
+
 function Admin() {
-    return ( <div>
-        asdjhhsa
-    </div> );
+    var s
+    function encode(){
+    var  element=document.getElementById("addimage")
+        var file = element.files[0];
+        var reader = new FileReader();
+        reader.onloadend = function() {
+          s=reader.result;
+          console.log(s)
+        }
+        reader.readAsDataURL(file);
+    }
+      
+    return (
+     <div>
+        <center>
+                <input className="inputs" type="text" id="addtitle" placeholder="product name"/>
+                <input className="inputs" type="text" id="price" placeholder="price"/>
+                <input type="file" id="addimage" onChange={()=>{encode()}}/>
+                <button className="inputs" id="additembtn">Add Product</button>
+        </center>
+    </div> 
+    );
 }
 
 export default Admin;
